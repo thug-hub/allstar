c772a475e5f5f166e317ea997d09ba688e21b1b4
thug-hub <107668369+thug-hub@users.noreply.github.com>
2023-01-21 17:50:34 -0600

    Create autoplaynext.lua

---
+++ autoplaynext.lua
@@ -0,0 +1,10 @@
+repeat task.wait() until game:GetService("Players").LocalPlayer.PlayerGui.HUD.MissionEnd.BG.Actions.Next.Visible == true
+task.wait(2)
+local VIM = game:GetService("VirtualInputManager")
+local clickGui = function(gui)
+    for i = 1, 2 do
+        VIM:SendMouseButtonEvent(gui.AbsolutePosition.X + gui.AbsoluteSize.X / 2, gui.AbsolutePosition.Y + 50, 0, ({true, false})[i], THUG_HUB, 1)
+    end
+end
+
+clickGui(game:GetService("Players").LocalPlayer.PlayerGui.HUD.MissionEnd.BG.Actions.Next)
