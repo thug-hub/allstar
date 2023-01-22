c6b68951a926ac3a218f5073d0928bc0d5f50ba1
thug-hub <107668369+thug-hub@users.noreply.github.com>
2023-01-21 18:01:05 -0600

    Update autoplaynext.lua

--- autoplaynext.lua
+++ autoplaynext.lua
@@ -1,11 +1,13 @@
-print("auto next activated")
-repeat task.wait() until game:GetService("Players").LocalPlayer.PlayerGui.HUD.MissionEnd.BG.Actions.Next.Visible == true
-task.wait(2)
-local VIM = game:GetService("VirtualInputManager")
-local clickGui = function(gui)
-    for i = 1, 2 do
-        VIM:SendMouseButtonEvent(gui.AbsolutePosition.X + gui.AbsoluteSize.X / 2, gui.AbsolutePosition.Y + 50, 0, ({true, false})[i], THUG_HUB, 1)
+if game.PlaceId == 4996049426 then
+    print("auto next activated")
+    repeat task.wait() until game:GetService("Players").LocalPlayer.PlayerGui.HUD.MissionEnd.BG.Actions.Next.Visible == true
+    task.wait(2)
+    local VIM = game:GetService("VirtualInputManager")
+    local clickGui = function(gui)
+        for i = 1, 2 do
+            VIM:SendMouseButtonEvent(gui.AbsolutePosition.X + gui.AbsoluteSize.X / 2, gui.AbsolutePosition.Y + 50, 0, ({true, false})[i], THUG_HUB, 1)
+        end
     end
+    
+    clickGui(game:GetService("Players").LocalPlayer.PlayerGui.HUD.MissionEnd.BG.Actions.Next)
 end
-
-clickGui(game:GetService("Players").LocalPlayer.PlayerGui.HUD.MissionEnd.BG.Actions.Next)
